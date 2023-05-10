fc287f890c7701deda2792a29247672a3ec032d0
Gilbert Gotora <gilbertgotora2@gmail.com>
2023-05-10 04:15:28 +0200

    create MenuItem model and API

--- API/views.py
+++ API/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 # Create your views here.
 def index(request):
     # return HttpResponse('Eish')
     return render(request, 'API/index.html')
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
